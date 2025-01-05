c4e2bea7ba8357156e468c1eab3c9e02a95fca08
cstephanielee <cstephanielee@yahoo.co.uk>
2025-01-05 17:07:54 +0800

    Added test pin verification

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from bankAccount import BankAccount
 
+def pinVerification(pin):
+    if pin == '1234':
+        return True
+    else:
+        return False
+
 if __name__ == '__main__':
 
     testAccount = BankAccount(id='BA123', surname='Smith', first_name='Bob', pin='1234', balance=50000)
 
     atmOn = True
-    if atmOn:
+    while atmOn:
         print("Welcome, please insert your card")
+        print("*Card inserted*")
+        print("* Test pin is 1234 *")
+        pin = input("Please input your pin: ")
+        verify = pinVerification(pin)
+        print(verify)
+
+
